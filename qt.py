8ca123872969040aca8a75d3d309a48c87d51a8b
Xenoshell <34055681+Xenoshell@users.noreply.github.com>
2017-12-13 09:21:27 +0100

    Update qt.py

--- qt.py
+++ qt.py
@@ -1,4 +1,4 @@
-import sys
+ import sys
 from PyQt5 import QtCore , QtWidgets, QtGui, QtMultimedia, QtMultimediaWidgets
 from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow
@@ -9,40 +9,46 @@ from PyQt5.QtMultimediaWidgets import QCameraViewfinder
 class Camera(QObject):
     def __init__(self, parent = QObject()):
         super(Camera, self).__init__(parent)
-        self.cam = QCamera()
+        print("3")
+        self.cam = QCamera(QCameraInfo.defaultCamera())   #"/dev/video0".encode()
+        print("4")
         self.caminfo = QCameraInfo(self.cam)
         self.camvfind = QCameraViewfinder()
         self.camvfindset = QCameraViewfinderSettings()
-        self.cammode = self.cam.CaptureMode(1)
+        self.cammode = self.cam.CaptureMode(0)
         self.camimgcap = QCameraImageCapture(self.cam)
 
     def iniCamera(self):
         print(self.caminfo.description())
         print(self.caminfo.availableCameras())
-        
+
+        for caminfo in QCameraInfo.availableCameras():
+            print(caminfo.deviceName())
+
         if self.cam.isCaptureModeSupported(self.cammode):
             print("Capturemode supported")
-            
-    
+
     def startVid(self):
-        self.camimgcap.CaptureDestination(2)
-        
-        self.camvfind.setVisible(True)
-        
+        #self.camimgcap.CaptureDestination(2)
+
+        self.camvfind.show()
+
         self.cam.setViewfinder(self.camvfind)
 
         self.cam.setCaptureMode(self.cammode)
+
         self.cam.start()
-        
-        
+
+
 
 if __name__ == '__main__':
+    print("1")
     app = QtWidgets.QApplication(sys.argv)
-    
+    print("2")
     cam = Camera()
-    
+    print("4")
     cam.iniCamera()
-    
+
     cam.startVid()
-    
+
     sys.exit(app.exec_())
